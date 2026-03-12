107523c68be045d5748c9aeffa704dda4287fc10
Jesus-Barraza <190627332+Jesus-Barraza@users.noreply.github.com>
2026-03-12 14:53:37 -0600

    Feat: fin de la actividad 1
    Si... Ya acabó esto

--- P3/src/js/act1.js
+++ P3/src/js/act1.js
@@ -1,9 +1,73 @@
+/*
+function calcularCobro(horas) {
+    return 0
+};
+*/
+
+/*
 function calcularCobro(horas) {
     if (horas > 0) {
         return 30 * horas
     } else {
         return 0
     } 
+};
+*/
+
+/*
+function calcularCobro(horas) {
+    if (horas > 0 && horas <= 2) {
+        return 30 * horas
+    } else if (horas > 2) {
+        return 25 * horas
+    } else {
+        return 0
+    } 
+};
+*/
+
+/*
+function calcularCobro(horas) {
+    if (horas > 0 && horas <= 2) {
+        return 30 * horas
+    } else if (horas > 2 && horas <= 5) {
+        return 25 * horas
+    } else if (horas > 5) {
+        return 20 * horas
+    } else {
+        return 0
+    } 
+};
+*/
+
+/*
+function calcularCobro(horas) {
+    if (horas > 0 && horas <= 2) {
+        return 30 * horas
+    } else if (horas > 2 && horas <= 5) {
+        return 25 * horas
+    } else if (horas > 5) {
+        return 20 * horas
+    } else if (horas == 0) {
+        return 0
+    } else {
+        return "Error: no se puede hacer horas negativas"
+    }
+};
+*/
+
+function calcularCobro(horas) {
+    if (horas > 0 && horas <= 2) {
+        return 30 * horas
+    } else if (horas > 2 && horas <= 5) {
+        return 25 * horas
+    } else if (horas > 5) {
+        return 20 * horas
+    } else if (horas == 0) {
+        return 0
+    } else {
+        throw new Error("no se puede hacer horas negativas")
+    }
 };
 
 module.exports = {calcularCobro};
--- P3/test/act1.test.js
+++ P3/test/act1.test.js
@@ -7,3 +7,21 @@ test("0 horas corresponde a $0", () => {
 test("1-2 horas corresponde a $30", () => {
     expect(calcularCobro(2)).toBe(60);
 });
+
+test("3-5 horas corresponde a $25", () => {
+    expect(calcularCobro(4)).toBe(100);
+});
+
+test("Más de 5 horas corresponde a $20", () => {
+    expect(calcularCobro(6)).toBe(120);
+});
+
+test("No se pueden hacer horas negativas", () => {
+    expect(() => calcularCobro(-3)).toThrow(Error);
+});
+
+/*
+test("No se pueden hacer horas negativas", () => {
+    expect(calcularCobro(-3)).toBe("Error: no se puede hacer horas negativas");
+});
+*/
